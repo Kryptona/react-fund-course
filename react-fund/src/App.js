6094aeee2eec2385b444f0779f394882fb5d7147
Дарья <dasha.seredenko.1997@gmail.com>
2021-08-19 18:12:39 +0400

    Сортировка

--- react-fund/src/App.js
+++ react-fund/src/App.js
@@ -4,15 +4,17 @@ import {PostList} from "./components/PostList";
 import {MyButton} from "./components/UI/button/MyButton";
 import {MyInput} from "./components/UI/input/MyInput";
 import {PostForm} from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: "Javascript", body: "Description"},
-        {id: 2, title: "Javascript 2", body: "Description"},
-        {id: 3, title: "Javascript 3", body: "Description"},
+        {id: 1, title: "aa", body: "nn"},
+        {id: 2, title: "bb 2", body: "f"},
+        {id: 3, title: "cc 3", body: "a"},
     ])
 
-
+    const [seletedSort, setSelectedSort] = useState("");
+    
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
@@ -21,9 +23,26 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = sort => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+    
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                    value={seletedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value:'title', name: 'По названию'},
+                        {value:'body', name: 'По описанию'},
+                    ]}
+                />
+            </div>
             {posts.length
                 ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
                 : <h1 style={{textAlign: 'center'}}>
